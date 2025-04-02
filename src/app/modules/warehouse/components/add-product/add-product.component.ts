4e87d972d2a375153e970db63652438342421edb
isaacr24reyes <isaacr24reyes@gmail.com>
2025-04-01 22:22:45 -0500

    allow-images

--- src/app/modules/warehouse/components/add-product/add-product.component.ts
+++ src/app/modules/warehouse/components/add-product/add-product.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {UntypedFormGroup} from "@angular/forms";
-
+import Notiflix from 'notiflix';
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
@@ -17,8 +17,14 @@ export class AddProductComponent {
     const fileInput = event.target as HTMLInputElement;
     if (fileInput?.files?.[0]) {
       const file = fileInput.files[0];
-      const reader = new FileReader();
+      const allowedTypes = ['image/jpeg', 'image/png'];
+      if (!allowedTypes.includes(file.type)) {
+        // Mostrar el alert con Notiflix
+        Notiflix.Notify.failure('Solo se permiten archivos JPG y PNG');
+        fileInput.value = '';
+      }
 
+      const reader = new FileReader();
       reader.onload = () => {
         this.imagePreview = reader.result as string;
       };
