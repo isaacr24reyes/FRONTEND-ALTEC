11d6eb98e867dce2d213ebfbf56681db1bc5c2f3
Isaac Reyes <altecdevops@hotmail.com>
2025-08-16 22:59:47 -0500

    Auto-generated commit

---
+++ chunk-X5L3W5B4.js
@@ -0,0 +1,1 @@
+var o="dashboard",t="authentication",e="404",r="login",R="register",n="warehouse",s="product-quote",c="store",_="qr-generator";export{o as a,t as b,e as c,r as d,R as e,n as f,s as g,c as h,_ as i};
