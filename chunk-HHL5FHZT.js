c1375cf0e1e2271eb1258e0389e36d79b28828b8
Isaac Reyes <altecdevops@hotmail.com>
2025-09-27 23:16:16 -0500

    Auto-generated commit

---
+++ chunk-HHL5FHZT.js
@@ -0,0 +1,1 @@
+var t="dashboard",o="authentication",e="404",r="login",R="register",s="warehouse",n="product-quote",c="store",_="altec-points",p="user-admin";export{t as a,o as b,e as c,r as d,R as e,s as f,n as g,c as h,_ as i,p as j};
--- chunk-IHXC5BBL.js
+++
@@ -1,1 +0,0 @@
-var t="dashboard",o="authentication",e="404",r="login",R="register",s="warehouse",n="product-quote",c="store",p="altec-points";export{t as a,o as b,e as c,r as d,R as e,s as f,n as g,c as h,p as i};
